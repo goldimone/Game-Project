7c671ffdd3c6e718b78b12606de63f4cb06ddbdf
unknown <jerrydamoyan.jd@gmail.com>
2025-02-22 18:53:07 +0800

    Added new scenes and fixed collision on player and road

--- 3d-platformer/objects/player.gd
+++ 3d-platformer/objects/player.gd
@@ -1,28 +1,144 @@
 extends CharacterBody3D
 
+signal coin_collected
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export_subgroup("Components")
+@export var view: Node3D
 
+@export_subgroup("Properties")
+@export var movement_speed = 250
+@export var jump_strength = 10
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+var movement_velocity: Vector3
+var rotation_direction: float
+var gravity = 0
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+var previously_floored = false
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+var jump_single = true
+var jump_double = true
 
+var coins = 0
+
+@onready var particles_trail = $ParticlesTrail
+@onready var sound_footsteps = $SoundFootsteps
+@onready var model = $Character
+@onready var animation = $Character/AnimationPlayer
+
+# Functions
+
+func _physics_process(delta):
+
+	# Handle functions
+
+	handle_controls(delta)
+	handle_gravity(delta)
+
+	handle_effects(delta)
+
+	# Movement
+
+	var applied_velocity: Vector3
+
+	applied_velocity = velocity.lerp(movement_velocity, delta * 10)
+	applied_velocity.y = -gravity
+
+	velocity = applied_velocity
 	move_and_slide()
+
+	# Rotation
+
+	if Vector2(velocity.z, velocity.x).length() > 0:
+		rotation_direction = Vector2(velocity.z, velocity.x).angle()
+
+	rotation.y = lerp_angle(rotation.y, rotation_direction, delta * 10)
+
+	# Falling/respawning
+
+	if position.y < -10:
+		get_tree().reload_current_scene()
+
+
+
+
+	previously_floored = is_on_floor()
+
+# Handle animation(s)
+
+func handle_effects(delta):
+
+
+
+	if is_on_floor():
+		var horizontal_velocity = Vector2(velocity.x, velocity.z)
+		var speed_factor = horizontal_velocity.length() / movement_speed / delta
+		if speed_factor > 0.05:
+			if animation.current_animation != "RobotArmature|Walk":
+				animation.play("RobotArmature|Walk", 0.1)
+		elif Input.is_action_pressed("jump"):
+			animation.play("RobotArmature|Jump",0.1)
+			await get_tree().create_timer(.166).timeout
+			animation.seek(.166)
+			animation.pause()
+		elif animation.current_animation != "RobotArmature|Idle":
+			animation.play("RobotArmature|Idle", 0.1)
+	elif animation.current_animation != "RobotArmature|Jump":
+		animation.play("RobotArmature|Jump", 0.1,1.0,true)
+
+# Handle movement input
+
+func handle_controls(delta):
+
+	# Movement
+
+	var input := Vector3.ZERO
+
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_back")
+
+	
+
+	if input.length() > 1:
+		input = input.normalized()
+
+	movement_velocity = input * movement_speed * delta
+
+	# Jumping		
+	if Input.is_action_just_released("jump"):
+
+		if jump_single or jump_double:
+			jump()
+
+# Handle gravity
+
+func handle_gravity(delta):
+
+	gravity += 25 * delta
+
+	if gravity > 0 and is_on_floor():
+
+		jump_single = true
+		gravity = 0
+
+# Jumping
+
+func jump():
+
+
+	gravity = -jump_strength
+
+
+
+	if jump_single:
+		jump_single = false;
+		jump_double = true;
+	else:
+		jump_double = false;
+
+# Collecting coins
+
+func collect_coin():
+
+	coins += 1
+
+	coin_collected.emit(coins)
